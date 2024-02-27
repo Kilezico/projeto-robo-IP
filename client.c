b396850152f9c9dc72f8880838fbc2e35f7743e9
KileZico <jgrangeirocosta@gmail.com>
2024-02-27 20:35:50 -0300

    Corrigi alguns errinhos

--- client.c
+++ client.c
@@ -62,7 +62,7 @@ void movJointsVet(int sock, char *buffer, float pos[], int state)
     movJoints(sock, buffer, pos[0], pos[1], pos[2], pos[3], pos[4], pos[5], state);
 }
 
-char conversor(int i)
+char conversor(int i) // Para printar no terminal.
 {
     switch (i)
     {
@@ -83,6 +83,7 @@ void printando(int board[3][3])
     printf("---+---+---\n");
     printf(" %c | %c | %c\n", conversor(board[2][0]), conversor(board[2][1]), conversor(board[2][2]));
 }
+
 int vitoria(const int board[3][3])
 {
     // Determina se um jogador venceu, retorna 0 caso contrário.
@@ -99,17 +100,17 @@ int vitoria(const int board[3][3])
     return 0;
 }
 
-int minmax(int board[3][3], int jogador) // para a maquina vai ser jogador=1 e para vc é jogador=-1
+int minimax(int board[3][3], int jogador) // para a maquina vai ser jogador=1 e para vc é jogador=-1
 {
     int acabou = vitoria(board); // verifica se ja acabou ou não a partida
     if (acabou != 0)
         return acabou * jogador;
 
     int move_i = -1, move_j = -1;
-    int melhorpontuação = -2; // Movimentos que levam à derrota são preferidos a não fazer nada
+    int melhorpontuacao = -2; // Movimentos que levam à derrota são preferidos a não fazer nada
     // coloquei o score sendo =-2 pois se fosse maior que alguma das possibilidades poderia ocasionar problemas com o codigo, sendo assim ele é -2 apenas para inicializar
     int i, j;
-    int pontuação, ;
+    int pontuacao;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
@@ -117,14 +118,14 @@ int minmax(int board[3][3], int jogador) // para a maquina vai ser jogador=1 e p
             if (board[i][j] == 0) // se tiver livre para poder fazer jogadas, entra nesse if
             {
                 board[i][j] = jogador;
-                pontuação = -minmax(board, jogador * -1) // aqui começo a puxar a recursão do minmax
+                pontuacao = -minimax(board, jogador * -1); // aqui começo a puxar a recursão do minmax
                                                          // EXPLICAÇÂO
-                                                         /*Colocamos o "-" antes do minmax para poder inverter o valor da pontuação do jogador a frente, pois como faço jogador*-1 para poder trocar quem vai jogar no min e max
+                                                         /*Colocamos o "-" antes do minmax para poder inverter o valor da pontuacao do jogador a frente, pois como faço jogador*-1 para poder trocar quem vai jogar no min e max
                                                          se nao tivesse esse - o valor vinha positivo quando fosse para ser negativo, dessa forma fazendo com que o min e max perca eficiencia
                                                          Com esse pequeno ajuste quando ele for jogar para prever possibilidades do player não ira cofundir com a maquina e será feliz!*/
-                            if (melhorpontuação < pontuação)
+                if (melhorpontuacao < pontuacao)
                 {
-                    melhorpontuação = pontuação;
+                    melhorpontuacao = pontuacao;
                     move_i = i;
                     move_j = j;
                 }
@@ -134,7 +135,7 @@ int minmax(int board[3][3], int jogador) // para a maquina vai ser jogador=1 e p
     }
     if (move_i = -1)
         return 0;
-    return melhorpontuação;
+    return melhorpontuacao;
 }
 int movrobo(int board[3][3], int *roboi, int *roboj)
 { // A função movrobo é responsável por determinar qual será a próxima jogada do computador, colocando cada casa vazia como algo e chamando o minmax para poder simular o valor do tabuleiro
@@ -149,11 +150,11 @@ int movrobo(int board[3][3], int *roboi, int *roboj)
             if (board[i][j] == 0)
             {
                 board[i][j] = 1;
-                int pontuação = -minimax(board, -1);
+                int pontuacao = -minimax(board, -1);
                 board[i][j] = 0;
-                if (pontuação > maxpont)
+                if (pontuacao > maxpont)
                 {
-                    pontuação = pontuação;
+                    pontuacao = pontuacao;
                     move_i = i;
                     move_j = j;
                 }
@@ -162,9 +163,9 @@ int movrobo(int board[3][3], int *roboi, int *roboj)
     }
     *roboi = move_i;
     *roboj = move_j;
-    /
-        // Faz a jogada do computador
-        board[move_i][move_j] = 1;
+    
+    // Faz a jogada do computador
+    board[move_i][move_j] = 1;
 }
 
 int main(int argc, char *argv[])
@@ -173,20 +174,17 @@ int main(int argc, char *argv[])
     // variaveis para conexao com o robo NAO MEXER
     int sock = 0;
     char buffer[2048] = {0};
-    int jogador;
-    int rodada = 0, jogadap_i, jogadap_j, jogadac_i, jogadac_j;
 
     // conexao com o robo NAO MEXER
     initSocket(&sock, buffer);
 
     // Programe aqui ----------------------------------------------------
+    
+    int jogador;
+    int rodada = 0, jogadap_i, jogadap_j, jogadac_i, jogadac_j;
 
-    int casas[3][3];
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-            casas[i][j] = '0'; // Inicia todas as casas com 'n'
-    }
+    int casas[3][3] = {0}; // Inicia todas as casas com 0 
+
     float angulospos[3][3][6] = {
         {{-0.27, -0.46, -0.67, -0.16, -0.37, -0.15},
          {-0.05, -0.45, -0.72, 0.05, -0.41, -0.07},
@@ -203,36 +201,36 @@ int main(int argc, char *argv[])
 
     printf("Deseja jogar primeiro? (1)Sim (2)Não");
     scanf("%d", &jogador);
-    while (vitoria(casas) == 0)
+    for (int round = 0; round < 9 && vitoria(casas); round++)
     {
-
-        printf("Digite sua jogada na forma(x,y)");
-        scanf("(%d,%d)", &jogadap_i, &jogadap_j);
-        for (int round = 0; round < 9; round++)
+        if ((rodada + jogador) % 2 == 0)
         {
-            if ((rodada + jogador) % 2 == 0)
-            {
-                movrobo(casas, &jogadac_i, &jogadac_j);
-                movJointsVet(sock, buffer, anguloPecaOpen, OPEN);
-                delay(500);
-                movJointsVet(sock, buffer, anguloPecaClose, CLOSE);
-                delay(500);
-                movJointsVet(sock, buffer, anguloPecaOpen, CLOSE);
-                delay(1000);
-                movJointsVet(sock, buffer, angulospos[jogadac_i][jogadac_j], OPEN);
-                delay(200);
-            }
-            else
-            {
-                movJointsVet(sock, buffer, anguloPecaOpen, OPEN);
-                delay(500);
-                movJointsVet(sock, buffer, anguloPecaClose, CLOSE);
-                delay(500);
-                movJointsVet(sock, buffer, anguloPecaOpen, CLOSE);
-                delay(1000);
-                movJointsVet(sock, buffer, angulospos[jogadap_i][jogadap_j], OPEN);
-                delay(200);
-            }
+            // Pega jogada do robo
+            movrobo(casas, &jogadac_i, &jogadac_j);
+            // Move
+            movJointsVet(sock, buffer, anguloPecaOpen, OPEN);
+            delay(500);
+            movJointsVet(sock, buffer, anguloPecaClose, CLOSE);
+            delay(500);
+            movJointsVet(sock, buffer, anguloPecaOpen, CLOSE);
+            delay(1000);
+            movJointsVet(sock, buffer, angulospos[jogadac_i][jogadac_j], OPEN);
+            delay(200);
+        }
+        else
+        {
+            // Pega jogada do jogador
+            printf("Digite sua jogada na forma(x,y)");
+            scanf("(%d,%d)", &jogadap_i, &jogadap_j);
+            // Move  
+            movJointsVet(sock, buffer, anguloPecaOpen, OPEN);
+            delay(500);
+            movJointsVet(sock, buffer, anguloPecaClose, CLOSE);
+            delay(500);
+            movJointsVet(sock, buffer, anguloPecaOpen, CLOSE);
+            delay(1000);
+            movJointsVet(sock, buffer, angulospos[jogadap_i][jogadap_j], OPEN);
+            delay(200);
         }
     }
 
